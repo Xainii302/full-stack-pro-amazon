d9bd16387b73631015c1e5678d2692670ac0cef4
zain rafiq <zainrafiq176@gmail.com>
2022-12-16 12:21:11 +0500

    fght

---
+++ frontend/build/precache-manifest.0b4dfd9d17eb6066e36b11ee1d36f686.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc290beb69e3495760340c378f06651a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a95744c134f1d849c277",
+    "url": "/static/css/main.2988cb80.chunk.css"
+  },
+  {
+    "revision": "478603ddaccdb6f55609",
+    "url": "/static/js/2.8d545eff.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.8d545eff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a95744c134f1d849c277",
+    "url": "/static/js/main.7901f1ba.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
